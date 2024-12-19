f828040a649511635e3f07a621bca4a0f34163c9
Danfmawel <153481892+Danfmawel@users.noreply.github.com>
2024-12-19 23:39:20 +0500

    /

---
+++ my-app/src/shared/providers/MainProvider.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { type PropsWithChildren } from 'react'
+
+import { TanstackQueryProvider, ThemeProvider } from './index'
+
+/**
+ * Оборачивает дочерние элементы в провайдеры для управления состоянием, темами и уведомлениями.
+ *
+ * @param {PropsWithChildren<unknown>} props - Свойства компонента.
+ * @returns {JSX.Element} - Провайдеры с дочерними элементами.
+ */
+export function MainProvider({ children }: PropsWithChildren<unknown>) {
+	return (
+		<TanstackQueryProvider>
+			<ThemeProvider
+				attribute='class'
+				defaultTheme='light'
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</TanstackQueryProvider>
+	)
+}
